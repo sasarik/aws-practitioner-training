a82074fcb1deb344258bdf1d936795fab33da9fe
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2023-02-18 16:45:22 +0100

    feat(chore): Add react-cloudfront-app initials

---
+++ packages/react-cloudfront-app/vite.config.ts
@@ -0,0 +1,45 @@
+/// <reference types="vitest" />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  cacheDir: '../../node_modules/.vite/react-cloudfront-app',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+    },
+  },
+
+  plugins: [react()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [
+  //    viteTsConfigPaths({
+  //      root: '../../',
+  //    }),
+  //  ],
+  // },
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+    setupFiles: './src/setupTests.ts',
+  },
+});
