f7ba35343eb46b35509754bdf416a3b4f0f44a6d
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2023-02-20 15:44:56 +0100

    feat(Task3): APIGateway endpoint changed

--- packages/react-cloudfront-app/src/constants/apiRoutes.ts
+++ packages/react-cloudfront-app/src/constants/apiRoutes.ts
@@ -1,4 +1,4 @@
-const API_GATEWAY = 'https://pahsc7wuxd.execute-api.eu-west-1.amazonaws.com/dev';
+const API_GATEWAY = 'https://c6lk4tmf1g.execute-api.eu-west-1.amazonaws.com/dev';
 export const apiRoutes = {
   getAvailableProductsListUrl: () => `${API_GATEWAY}/products`,
 };
