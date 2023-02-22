4c2f2c4e3ff6a60a0d1a77c837e2d868c8766589
Oleksandr Radvanskyi <oleksandr_radvanskyi@epam.com>
2023-02-22 11:36:11 +0100

    feat(chore): Rollback models

--- packages/react-cloudfront-app/src/models/Order.ts
+++ packages/react-cloudfront-app/src/models/Order.ts
@@ -1,5 +1,5 @@
 import * as Yup from 'yup';
-import { OrderStatus } from './OrderStatus';
+import { OrderStatus } from '~/constants/order';
 
 export const AddressSchema = Yup.object({
   firstName: Yup.string().required().default(''),
