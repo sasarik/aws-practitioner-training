2958005d475b1659b95a30e41f9ca62edd0fdf3f
Sasarik <sasarik@gmail.com>
2023-03-14 12:31:22 +0100

    feat(Task-7): The "basicAuthorizer" lambda implementation

--- stacks/authorization-service/serverless.ts
+++ stacks/authorization-service/serverless.ts
@@ -2,7 +2,7 @@ import type { AWS } from '@serverless/typescript';
 import { baseServerlessConfiguration } from '../../serverless.base';
 import { basicAuthorizer } from './src/functions';
 
-const SERVICE_NAME = 'aws-training-authorization-service';
+const SERVICE_NAME = process.env.AUTHORIZATION_SERVICE_NAME;
 
 const serverlessConfiguration = <AWS>{
   ...baseServerlessConfiguration,
@@ -18,6 +18,17 @@ const serverlessConfiguration = <AWS>{
   functions: {
     basicAuthorizer,
   },
+  // Seems good idea to share/export the "BasicAuthorizerLambdaFunctionQualifiedArn",
+  // but it "works" only once, can't deploy the modified authorization-service then;
+  // Workaround ? https://stackoverflow.com/questions/54968683/updating-dependent-stacks
+  // resources: {
+  //   Outputs: {
+  //     BasicAuthorizerLambdaFunctionQualifiedArn: {
+  //       Description: 'The ApiGateway basic authorization Lambda function output',
+  //       Export: { Name: 'aws-training-BasicAuthorizerLambdaFunctionQualifiedArn' },
+  //     },
+  //   },
+  // },
 };
 
 module.exports = serverlessConfiguration;
--- stacks/authorization-service/src/functions/basic-authorizer/handler.ts
+++ stacks/authorization-service/src/functions/basic-authorizer/handler.ts
@@ -1,14 +1,37 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const main = async (_) => {
-  // TODO Authorization routines
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'My "basicAuthorizer" function executed successfully!',
-      },
-      null,
-      2
-    ),
-  };
+import {
+  assertIsValidBasicAuthorizerTrigger,
+  generateApiGatewayAuthorizerResult,
+  getBasicAuthorizerCredentials,
+  UnauthorizedError,
+} from '@helpers/authorization';
+import { APIGatewayRequestAuthorizerEventV2 } from 'aws-lambda/trigger/api-gateway-authorizer';
+
+export const main = async (event: APIGatewayRequestAuthorizerEventV2) => {
+  try {
+    console.log(`~~~~~ APIGatewayRequestAuthorizerEvent: `, event);
+    assertIsValidBasicAuthorizerTrigger(event);
+
+    const { username, password } = getBasicAuthorizerCredentials(event);
+
+    const storedUserPassword = process.env[username];
+    const Effect = !storedUserPassword || storedUserPassword !== password ? 'Deny' : 'Allow';
+    console.log(`~~~~~ Authorization of username: "${username}"; password: "${password}" -> "${Effect}"`);
+
+    return generateApiGatewayAuthorizerResult({
+      principalId: event.headers['authorization'],
+      Effect,
+      Resource: event.routeArn,
+    });
+  } catch (error) {
+    if (error instanceof UnauthorizedError) {
+      console.error('~~~~~ The UnauthorizedError occurred: ', error);
+    } else {
+      console.error('~~~~~ The Error occurred: ', error);
+    }
+    return generateApiGatewayAuthorizerResult({
+      principalId: event.headers['authorization'],
+      Effect: 'Deny',
+      Resource: event.routeArn,
+    });
+  }
 };
